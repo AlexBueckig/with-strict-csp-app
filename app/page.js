f12bf9e47c9ec02bedefeee3427861180a5fda3a
Alexander Bückig <alexander.bueckig@werkbank.de>
2024-03-11 14:11:06 +0100

    fix typo

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Page() {
 
   return (
     <main>
-      <h1>CSP Test </h1>
+      <h1>CSP Test</h1>
       <p>Nonce: {nonce}</p>
       <Script nonce={nonce}>{`console.log("CSP");`}</Script>
     </main>
